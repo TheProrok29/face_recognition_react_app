90a7844a4f567b4954133fd06bf09e0cbc6d6729
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2019-11-08 19:32:19 +0100

    Create Rank component

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react';
+
+const Rank = () => {
+    return (
+        <Fragment>
+            <div className='white f3'>
+                {'Tom, your current rank is...'}
+            </div>
+            <div className='white f1'>
+                {'#5'}
+            </div>
+        </Fragment>
+    );
+}
+
+export default Rank;
