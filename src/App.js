c384e28ffbd4ee0a5a8403a06ca5e47a42a57507
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2019-12-08 14:03:29 +0100

    Update user profile (entry count)

--- src/App.js
+++ src/App.js
@@ -75,26 +75,35 @@ class App extends Component {
   }
 
   displayFaceBox = (box) => {
-    this.setState({
-      box: box
-    })
+    this.setState({ box: box });
   }
 
   onInputChange = (event) => {
-    this.setState({
-      input: event.target.value
-    });
+    this.setState({ input: event.target.value });
   }
 
-  onButtonSubmit = () => {
-    this.setState({
-      imageUrl: this.state.input
-    });
+  onPictureSubmit = () => {
+    this.setState({ imageUrl: this.state.input });
     app.models
       .predict(
         Clarifai.FACE_DETECT_MODEL,
         this.state.input)
-      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .then(response => {
+        if (response) {
+          fetch('http://localhost:3000/image', {
+            method: 'put',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
+          })
+            .then(response => response.json())
+            .then(count => {
+              this.setState(Object.assign(this.state.user, { entries: count }))
+            })
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      })
       .catch(err => console.log(err));
   }
 
@@ -135,7 +144,7 @@ class App extends Component {
             <Rank name={this.state.user.name} entries={this.state.user.entries} />
             <ImageLinkForm
               onInputChange={this.onInputChange}
-              onButtonSubmit={this.onButtonSubmit}
+              onPictureSubmit={this.onPictureSubmit}
             />
             <FaceRecognition box={box} imageUrl={imageUrl} />
           </Fragment>
