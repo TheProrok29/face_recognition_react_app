da8816c90aa7aa087e5fe10ef196025fd383de61
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2019-12-17 11:36:51 +0100

    Add condition when image url is empty do nothing

--- src/App.js
+++ src/App.js
@@ -76,35 +76,37 @@ class App extends Component {
   }
 
   onPictureSubmit = () => {
-    this.setState({ imageUrl: this.state.input });
-    fetch('https://ancient-tor-21039.herokuapp.com/imageurl', {
-      method: 'post',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        input: this.state.input
+    if (this.state.input) {
+      this.setState({ imageUrl: this.state.input });
+      fetch('https://ancient-tor-21039.herokuapp.com/imageurl', {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          input: this.state.input
+        })
       })
-    })
-      .then(response => response.json())
-      .then(response => {
-        if (response) {
-          fetch('https://ancient-tor-21039.herokuapp.com/image', {
-            method: 'put',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-              id: this.state.user.id
+        .then(response => response.json())
+        .then(response => {
+          if (response) {
+            fetch('https://ancient-tor-21039.herokuapp.com/image', {
+              method: 'put',
+              headers: { 'Content-Type': 'application/json' },
+              body: JSON.stringify({
+                id: this.state.user.id
+              })
             })
-          })
-            .then(response => response.json())
-            .then(count => {
-              this.setState(Object.assign(this.state.user, { entries: count }))
-            })
-            .catch(console.log)
-
-        }
-        this.displayFaceBox(this.calculateFaceLocation(response))
-      })
-      .catch(err => console.log(err));
+              .then(response => response.json())
+              .then(count => {
+                this.setState(Object.assign(this.state.user, { entries: count }))
+              })
+              .catch(console.log)
+          }
+          this.displayFaceBox(this.calculateFaceLocation(response))
+        })
+        .catch(err => console.log(err));
+    }
   }
+
 
   onRouteChange = (route) => {
     if (route === 'signout') {
